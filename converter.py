ed93fb65e7c4babfc55eabdc3e28f341f652e335
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-01-29 09:12:36 -0800

    addes functionality to remove empty lines from the out text

--- converter.py
+++ converter.py
@@ -1,5 +1,7 @@
 import sys 
 import os 
+import re
+
 
 starts_with = "<?php include_once('2015_templates/_includes/header.php') ?>\n" + \
               "<?php include_once('2015_templates/_includes/nav.php') ?>\n"
@@ -11,12 +13,20 @@ for root, dirs, files in os.walk(".", topdown=True):
     for name in files:
         if (name.endswith('.html') or name.endswith('.php')):
             inFile = open(os.path.join(root, name))
-            os.rename(os.path.join(root, name), os.path.join(root, '_'+name))
             text = inFile.read()
-            startIndex = text.find("<!-- startprint -->") 
-            stopIndex = text.find("<!-- stopprint -->") 
-            newText = starts_with + text[(startIndex+len("<!-- startprint -->")):stopIndex] + ends_with 
+            inFile.close()
+            os.rename(os.path.join(root, name), os.path.join(root, '_'+name))
+
+            startIndex = text.find("<!-- startprint -->")
+            if startIndex == -1:
+				continue 
+            stopIndex = text.find("<!-- stopprint -->")
+           	''' Im assuming if there is a start tag there will also be an end tag '''
+            newText = starts_with + text[(startIndex+len("<!-- startprint -->")):stopIndex] + ends_with
+            filtered = filter(lambda x: not re.match(r'^\s*$', x), newText)
+
             outFile = open(os.path.join(root, name.split('.')[0] + ".php"), "w") 
-            outFile.write(newText) 
+            outFile.write(newText)
+            outFile.close() 
         else: 
             continue
