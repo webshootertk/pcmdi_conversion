439f4ebb2c7ba36b43353fe7473f18ca23caac0b
sterlingbaldwin <baldwin32@llnl.gov>
2015-02-19 10:39:42 -0800

    polished the converter and deconverter

--- converter.py
+++ converter.py
@@ -5,11 +5,27 @@ import re
 
 starts_with = "<?php include_once('2015_templates/_includes/header.php') ?>\n" + \
               "<?php include_once('2015_templates/_includes/nav.php') ?>\n"
-ends_with =   "<?php include_once('2015_templates/_includes/footer.php') ?>"
+ends_with =   "\n<?php include_once('2015_templates/_includes/footer.php') ?>"
 
+file_count = 0
+
+def writeOut(text, name):
+    #file_count += 1
+    text = starts_with + text + ends_with
+    filtered = filter(lambda x: not re.match(r'^\s*$', x), text)
+    outFile = open(os.path.join(root, name.split('.')[0] + '.php'), 'w') 
+    outFile.write(text)
+    outFile.close()
+
+def writeReview(name, root):
+    print 'Adding ' + name + ' to manual review'
+    listFile = open(os.path.join('.', 'manual_review.txt'), 'a')
+    listFile.write(os.path.join(root,name) + '\n')
+    listFile.close()
 
 
 for root, dirs, files in os.walk(".", topdown=True):
+    
     for name in files:
         if (name.endswith('.html') or name.endswith('.php')):
             inFile = open(os.path.join(root, name))
@@ -19,14 +35,16 @@ for root, dirs, files in os.walk(".", topdown=True):
 
             startIndex = text.find("<!-- startprint -->")
             if startIndex == -1:
-				continue 
+                newText = text
+                writeReview(name, root)
+                writeOut(newText, name)
+                continue
             stopIndex = text.find("<!-- stopprint -->")
-           	''' Im assuming if there is a start tag there will also be an end tag '''
-            newText = starts_with + text[(startIndex+len("<!-- startprint -->")):stopIndex] + ends_with
-            filtered = filter(lambda x: not re.match(r'^\s*$', x), newText)
-
-            outFile = open(os.path.join(root, name.split('.')[0] + ".php"), "w") 
-            outFile.write(newText)
-            outFile.close() 
+            newText = text[(startIndex+len("<!-- startprint -->")):stopIndex]
+            writeOut(newText, name)
         else: 
             continue
+
+
+print "total file count: " + str(file_count)
+
--- deconvert.py
+++ deconvert.py
@@ -4,7 +4,18 @@ import os
 
 for root, dirs, files in os.walk(".", topdown=True):
 	for name in files:
-		if os.path.join(root, name).startswith('_'):
+		if name[0] == '_':
+			print name
+		elif name[-4:] == '.php':
+				os.remove(os.path.join(root, name))
+				print 'removing ' + name
+
+for root, dirs, files in os.walk(".", topdown=True):
+	for name in files:
+		if name[0] == '_':
 			os.rename(os.path.join(root, name), os.path.join(root, name[1:]))
 		else: 
 			continue
+
+if os.path.isfile('manual_review.txt'):
+	os.remove('manual_review.txt')
