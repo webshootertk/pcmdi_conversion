49bbf5a804c22a172ee4260d96a9a9c2f0da8b36
Matthew Harris <mattbenharris@gmail.com>
2015-02-19 15:08:25 -0800

    MBH: adding in nav

--- 2015_templates/_includes/nav.php
+++ 2015_templates/_includes/nav.php
@@ -8,25 +8,78 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Bootstrap theme</a>
+          <a class="navbar-brand" href="index.php">PCMDI</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
+                <li><a href="about/index.php">About PCMDI</a></li>
+                <li><a href="about/staff.php">Staff Directory</a></li>
+                <li><a href="about/locations.php">Locations</a></li>
               </ul>
             </li>
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">WCRP CMIP3<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="ipcc/about_ipcc.php">About WCRP CMIP3 Output</a></li>
+                <li><a href="ipcc/climate_forcing.php">Climate Forcing</a></li>
+                <li><a href="ipcc/data_status_tables.php">Data Status</a></li>
+                <li><a href="https://esgcet.llnl.gov:8443/index.jsp">Data Portal</a></li>
+                <li><a href="ipcc/diagnostic_subprojects.php">Diagnostic Subprojects</a></li>
+                <li><a href="ipcc/subproject_publications.php">SubProjects Publications</a></li>
+              </ul>
+            </li>
+
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Projects<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="projects/model_intercomparison.php">Model Intercomparison</a></li>
+                <li><a href="projects/model_testbed.php">Model Testbed</a></li>
+                <li><a href="projects/detection_attribution.php">Detection / Attribution</a></li>
+                <li><a href="projects/pcmdi/index.php">PCMDI</a></li>
+                <li><a href="projects/capt/index.php">CAPT</a></li>
+                <li><a href="projects/cloud_feedbacks/index.php">Cloud Feedbacks</a></li>
+                <li><a href="#">CMIP6</a></li>
+                <li><a href="http://cmip-pcmdi.llnl.gov/cmip5/">CMIP5</a></li>
+                <li><a href="ipcc/about_ipcc.php">CMIP3</a></li>
+                <li><a href="projects/model_intercomparison.php">Other MIPS</a></li>
+              </ul>
+            </li>
+
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Software<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="http://www2-pcmdi.llnl.gov">About Software</a></li>
+                <li><a href="https://aims.llnl.gov">AIMS</a></li>
+                <li><a href="http://esgf.llnl.gov">ESGF</a></li>
+                <li><a href="http://pcmdi9.llnl.gov/esgf-web-fe/">ESGF Node</a></li>
+                <li><a href="http://uvcdat.llnl.gov">UV-CDAT</a></li>
+                <li><a href="#">CDAT</a></li>
+                <li><a href="#">CMOR</a></li>
+                <li><a href="http://cfconventions.org">CF Conventions</a></li>
+              </ul>
+            </li>
+
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Publications<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="publications/index.php">About Publications</a></li>
+                <li><a href="publications/reports.php">Report Series</a></li>
+                <li><a href="publications/reviewed_literature.php">Reviewd Literature</a></li>
+              </ul>
+            </li>
+
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Calendar<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="calendar.php">Google</a></li>
+                <li><a href="lab_calendar.php">LLNL</a></li>
+              </ul>
+            </li>
+
           </ul>
         </div><!--/.nav-collapse -->
       </div>
