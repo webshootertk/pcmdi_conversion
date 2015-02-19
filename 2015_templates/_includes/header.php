3fc7035f83d7205fa58471ea843f1ced5782fbb4
Matthew Harris <mattbenharris@gmail.com>
2015-02-19 15:45:44 -0800

    MBH :correcting path issues

--- 2015_templates/_includes/header.php
+++ 2015_templates/_includes/header.php
@@ -11,24 +11,24 @@
     <title> <?php echo __FILE__; ?> </title>
 
     <!-- Bootstrap core CSS -->
-    <link href="2015_templates/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/2015_templates/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap theme -->
-    <link href="2015_templates/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="/2015_templates/css/bootstrap-theme.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="2015_templates/css/theme.css" rel="stylesheet">
-    <link href="2015_templates/css/nav-hover.css" rel="stylesheet">
+    <link href="/2015_templates/css/theme.css" rel="stylesheet">
+    <link href="/2015_templates/css/nav-hover.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="2015_templates/js/ie-emulation-modes-warning.js"></script>
+    <script src="/2015_templates/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <?php include_once('2015_templates/_includes/analytics.php') ?>
+    <?php include_once('/var/www/pcmdi-site/2015_templates/_includes/analytics.php') ?>
   </head>
 
   <body role="document">
--- 2015_templates/_includes/nav.php
+++ 2015_templates/_includes/nav.php
@@ -15,37 +15,37 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="about/index.php">About PCMDI</a></li>
-                <li><a href="about/staff.php">Staff Directory</a></li>
-                <li><a href="about/locations.php">Locations</a></li>
+                <li><a href="/about/index.php">About PCMDI</a></li>
+                <li><a href="/about/staff.php">Staff Directory</a></li>
+                <li><a href="/about/locations.php">Locations</a></li>
               </ul>
             </li>
 
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">WCRP CMIP3<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="ipcc/about_ipcc.php">About WCRP CMIP3 Output</a></li>
-                <li><a href="ipcc/climate_forcing.php">Climate Forcing</a></li>
-                <li><a href="ipcc/data_status_tables.php">Data Status</a></li>
+                <li><a href="/ipcc/about_ipcc.php">About WCRP CMIP3 Output</a></li>
+                <li><a href="/ipcc/climate_forcing.php">Climate Forcing</a></li>
+                <li><a href="/ipcc/data_status_tables.php">Data Status</a></li>
                 <li><a href="https://esgcet.llnl.gov:8443/index.jsp">Data Portal</a></li>
-                <li><a href="ipcc/diagnostic_subprojects.php">Diagnostic Subprojects</a></li>
-                <li><a href="ipcc/subproject_publications.php">SubProjects Publications</a></li>
+                <li><a href="/ipcc/diagnostic_subprojects.php">Diagnostic Subprojects</a></li>
+                <li><a href="/ipcc/subproject_publications.php">SubProjects Publications</a></li>
               </ul>
             </li>
 
           <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Projects<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="projects/model_intercomparison.php">Model Intercomparison</a></li>
-                <li><a href="projects/model_testbed.php">Model Testbed</a></li>
-                <li><a href="projects/detection_attribution.php">Detection / Attribution</a></li>
-                <li><a href="projects/pcmdi/index.php">PCMDI</a></li>
-                <li><a href="projects/capt/index.php">CAPT</a></li>
-                <li><a href="projects/cloud_feedbacks/index.php">Cloud Feedbacks</a></li>
+                <li><a href="/projects/model_intercomparison.php">Model Intercomparison</a></li>
+                <li><a href="/projects/model_testbed.php">Model Testbed</a></li>
+                <li><a href="/projects/detection_attribution.php">Detection / Attribution</a></li>
+                <li><a href="/projects/pcmdi/index.php">PCMDI</a></li>
+                <li><a href="/projects/capt/index.php">CAPT</a></li>
+                <li><a href="/projects/cloud_feedbacks/index.php">Cloud Feedbacks</a></li>
                 <li><a href="#">CMIP6</a></li>
                 <li><a href="http://cmip-pcmdi.llnl.gov/cmip5/">CMIP5</a></li>
-                <li><a href="ipcc/about_ipcc.php">CMIP3</a></li>
-                <li><a href="projects/model_intercomparison.php">Other MIPS</a></li>
+                <li><a href="/ipcc/about_ipcc.php">CMIP3</a></li>
+                <li><a href="/projects/model_intercomparison.php">Other MIPS</a></li>
               </ul>
             </li>
 
@@ -66,17 +66,17 @@
           <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Publications<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="publications/index.php">About Publications</a></li>
-                <li><a href="publications/reports.php">Report Series</a></li>
-                <li><a href="publications/reviewed_literature.php">Reviewd Literature</a></li>
+                <li><a href="/publications/index.php">About Publications</a></li>
+                <li><a href="/publications/reports.php">Report Series</a></li>
+                <li><a href="/publications/reviewed_literature.php">Reviewd Literature</a></li>
               </ul>
             </li>
 
           <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Calendar<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="calendar.php">Google</a></li>
-                <li><a href="lab_calendar.php">LLNL</a></li>
+                <li><a href="/calendar.php">Google</a></li>
+                <li><a href="/lab_calendar.php">LLNL</a></li>
               </ul>
             </li>
 
