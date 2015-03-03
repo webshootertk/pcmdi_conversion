f420741ab9322dc854ec184a0134df407b0fa866
Matthew Harris <mattbenharris@gmail.com>
2015-03-03 08:52:55 -0800

    MBH: reverting

--- converter.py
+++ converter.py
@@ -3,10 +3,15 @@ import os
 import re
 
 
+starts_with = "<?php include_once('/var/www/pcmdi-site/2015_templates/_includes/header.php') ?>\n" + \
+              "<?php include_once('/var/www/pcmdi-site/2015_templates/_includes/nav-www-pcmdi.php') ?>\n"
+ends_with =   "\n<?php include_once('/var/www/pcmdi-site/2015_templates/_includes/footer.php') ?>"
+
 file_count = 0
 
 def writeOut(text, name):
     #file_count += 1
+    text = starts_with + text + ends_with
     filtered = filter(lambda x: not re.match(r'^\s*$', x), text)
     outFile = open(os.path.join(root, name.split('.')[0] + '.php'), 'w') 
     outFile.write(text)
