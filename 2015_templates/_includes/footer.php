7441e2e2cd7a5550166ea3f59d127beb672c1772
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-04-07 15:07:20 -0700

    I'm a little tea cup

---
+++ 2015_templates/_includes/footer.php
@@ -0,0 +1,27 @@
+    <hr>
+    </div>
+    <footer class="footer">
+      <div class="container">
+        <div class-"well well-sm">
+          <a href="http://www.llnl.gov"><img src="http://www-pcmdi.llnl.gov/images/logo_llnl.jpg" width="60" height="60" border="1"></a> &nbsp;
+          <a href="http://www.science.energy.gov/ber/"><img src="http://www-pcmdi.llnl.gov/images/logo_ber.jpg" width="60" height="60" border="1"></a> &nbsp;
+          <a href="http://science.energy.gov/"><img src="http://www-pcmdi.llnl.gov/images/DOE_SC_Vertical_60h.png" width="164" height="60" border="1"></a> &nbsp;
+          <a href="http://www.llnl.gov/disclaimer.html" class="sideNav">Privacy &amp; Legal Notice</a> &nbsp;
+          <a href="https://www-pls.llnl.gov/" class="text">Physical  &amp; Life Sciences Directorate </a> &nbsp;
+          <a href="#" class="text">UCRL-WEB-152471</a> &nbsp;
+          <a href="/sitemap.php" class="text">Site Map</a> &nbsp;
+          <a href="/Links/links.php" class="text">Links</a>
+        </div>
+      </div>
+      </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="/2015_templates/js/bootstrap.min.js"></script>
+    <script src="/2015_templates/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/2015_templates/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
